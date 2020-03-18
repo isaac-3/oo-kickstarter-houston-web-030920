cd2b32d97c37ddd9da5a7db39260c16bd632b651
isaac-3 <isaacgc0596@gmail.com>
2020-03-18 09:40:56 -0500

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,19 @@
+class Project
+    attr_reader :title, :backers
+
+    def initialize(title)
+        @title = title
+        @backers = []
+    end
+
+    def add_backer(backer)
+        if @backers.include?(backer)
+            nil
+        else
+            @backers << backer
+            backer.back_project(self)
+        end
+    end
+
+
+end
